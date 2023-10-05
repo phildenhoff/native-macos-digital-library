a5bf15f1a1a8befe10e958ca73d0b4ee7afa2049
Phil Denhoff <phil@denhoff.ca>
2023-10-04 23:25:18 -0700

    Initial Commit

---
+++ book-reader/book_readerApp.swift
@@ -0,0 +1,17 @@
+//
+//  book_readerApp.swift
+//  book-reader
+//
+//  Created by Phil Denhoff on 2023-10-04.
+//
+
+import SwiftUI
+
+@main
+struct book_readerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ book-readerUITests/book_readerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  book_readerUITestsLaunchTests.swift
+//  book-readerUITests
+//
+//  Created by Phil Denhoff on 2023-10-04.
+//
+
+import XCTest
+
+final class book_readerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
